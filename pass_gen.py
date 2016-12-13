a0d17210c82d2d3009bf8bd212aa8adf4ae1c5d8
Radosław Frąckiewicz <frodoslaw@gmail.com>
2016-12-13 12:56:38 +0100

    Add files via upload
    Basic password generator

---
+++ pass_gen.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+#Basic password generator
+
+import string, random
+def passgenerator():
+    chars = string.digits + string.ascii_letters + '!@#$%^&*()'
+    pass_length = 13
+    password = ""
+
+    for char in range(pass_length):
+        next_index = random.randrange(len(chars))
+        password = password + chars[next_index]
+    return password
+
+print(passgenerator())
